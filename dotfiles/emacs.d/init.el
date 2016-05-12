7d4b67a3122bed8e3c92a0887c77c304da45996c
Alex Vasyutin <alex.vasyutin@gmail.com>
2016-05-12 12:43:41 +0200

    Add automatically installing packages for emacs

---
+++ dotfiles/emacs.d/init.el
@@ -0,0 +1,33 @@
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not. 
+  Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+	 nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+	   (package-install package)
+	 package)))
+   packages))
+
+(require 'package)
+
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib.
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+;; Make sure to have downloaded archive description.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+(package-initialize)
+
+(ensure-package-installed 'evil
+			  'helm
+			  'projectile)
+
+(evil-mode 1)
+
+  
