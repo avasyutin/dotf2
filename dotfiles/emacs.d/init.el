47675bf0d914ed0ede029a387db825f09879ca53
Alex Vasyutin <alex.vasyutin@gmail.com>
2016-05-19 23:02:33 +0200

    WIP emacs config

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -4,17 +4,17 @@
   (mapcar
    (lambda (package)
      (if (package-installed-p package)
-	 nil
+         nil
        (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-	   (package-install package)
-	 package)))
+           (package-install package)
+         package)))
    packages))
 
 (require 'package)
 
 (add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib.
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
@@ -26,14 +26,14 @@
 (package-initialize)
 
 (ensure-package-installed 'evil
-			  'helm
-			  'projectile
-                          'monokai-theme
+                          'helm
+                          'projectile
                           'powerline
-                          'powerline-evil
                           'moe-theme
-                          'smart-mode-line)
+                          'indent-guide
+                          'fill-column-indicator)
 
+;; Evil mode settings
 (evil-mode 1)
 
 ;; Stop littering everywhere with save files, put them somewhere
@@ -43,7 +43,6 @@
 (setq-default indent-tabs-mode nil)
 
 ;; Hide menu bar
-
 (menu-bar-mode -1)
 
 ;; Theme UI and colors
@@ -55,12 +54,6 @@
 (require 'powerline)
 (powerline-evil-center-color-theme)
 (powerline-moe-theme)
-; (setq sml/theme 'powerline)
-; (setq sml/mode-width 0)
-; (setq sml/name-width 20)
-; (rich-minority-mode 1)
-; (sml/setup)
-; (set-face-attribute 'default nil :height 100)
 
 ;; display line numbers
 (global-linum-mode 1)
@@ -68,43 +61,17 @@
 ;; Changes all yes/no questions to y/n type
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;;(global-whitespace-mode t)
-
-;; make whitespace-mode use just basic coloring
-;;(setq whitespace-style '(space tabs newline space-mark tab-mark newline-mark lines-tail trailing))
-
-;; make whitespace-mode use just basic coloring
-;; (setq whitespace-style '(spaces tabs newline space-mark tab-mark newline-mark))
-
-;;(setq whitespace-display-mappings
-      ;; all numbers are Unicode codepoint in decimal. try (insert-char 182 ) to see it
-;;      '(
-;;        (space-mark 32 [183] [46]) ; 32 SPACE, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-;;        (newline-mark 10 [182 10]) ; 10 LINE FEED
-;;        (tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
-;;                ))
-
-;;(custom-set-faces
-;; '(whitespace-space
-;;   ((((class color) (background dark)) (:foreground "#363636"))
-;;    (((class color) (background light)) (:background "yellow" :foreground "black"))
-;;        (t (:inverse-video t)))))
-
-;;(set-face-attribute 'whitespace-space nil :background nil :foreground "#363636")
-;;(set-face-attribute 'whitespace-newline nil :background nil :foreground "#363636")
-
-;;(set-face-attribute
-;; 'whitespace-space-after-tab nil
-;; :background "#363636"
-;; :foreground (face-attribute 'default :background))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(comment-empty-lines t)
+ ;;'(fci-rule-character-color "color-238")
  '(global-whitespace-mode t)
  '(global-whitespace-newline-mode t)
  '(powerline-default-separator (quote shape))
+ '(require-final-newline t)
  '(whitespace-display-mappings
    (quote
     ((space-mark 32
@@ -113,18 +80,28 @@
      (space-mark 160
                  [164]
                  [95])
-               ))))
+     (tab-mark 9
+               [9655 9]
+               [92 9])))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(whitespace-empty ((t (:foreground "color-240"))))
+ '(whitespace-empty ((t (:background "color-237"))))
  '(whitespace-line ((t nil)))
  '(whitespace-newline ((t nil)))
- '(whitespace-space ((t (:foreground "color-240"))))
- '(whitespace-space-after-tab ((t (:foreground "color-240"))))
- '(whitespace-trailing ((t (:foreground "color-240")))))
+ '(whitespace-space ((t (:foreground "color-238"))))
+ '(whitespace-space-after-tab ((t (:foreground "color-238"))))
+ '(whitespace-trailing ((t (:foreground "color-238")))))
 
+;; Indent guides
+(indent-guide-global-mode)
+(setq indent-guide-char "⁞")
+(setq indent-guide-recursive t)
 
+;; 80's column indicator
+(add-hook 'ruby-mode-hook 'fci-mode)
+(setq fci-rule-column 100)
 
+(add-hook 'before-save-hook 'whitespace-cleanup 'untabify)
